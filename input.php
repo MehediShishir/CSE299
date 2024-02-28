cee583717d940ca2eb79a4977819a75874bc26e4
Sadman Islam Chowdhury Samin <sammyshade664@gmail.com>
2024-02-28 10:21:47 +0600

    Updated input.php

--- input.php
+++ input.php
@@ -123,7 +123,7 @@ function send_form_to_emails($info_data, $email_column, $form_id, $deadline)
   <div class="navbar-brand">
     <a class="navbar-item" href="https://bulma.io">
       <img src="images/logo.jpg" >
-      <h1 class="title is-size-4 has-text-white">  Automated Assessment System (AAS)</h1>
+      <h1 class="title is-size-4 has-text-white">    Automated Assessment System (AAS)</h1>
     </a>
 
     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
@@ -146,19 +146,73 @@ function send_form_to_emails($info_data, $email_column, $form_id, $deadline)
   </div>
 </nav>
 <nav>
+
+
+<nav>
 <div class="coloumns mr-6">
     <div class="coloum">
-        <div class="class">
-            <header class="card-header title"> </header>
-            <div class="card-content">
+        <div class="card mx-auto" style="width: 30%;">
+        
+            <header class="card-header text-center title "> 
+            <center>
             <?php
-                            echo"<center><h1><font color= black>Welcome " . $_SESSION['name'] . "</font></h1></center>";
+                            echo"<h1><font color= grey>Welcome " . $_SESSION['name'] . "</font></h1>";
                             
                             ?>
+                            </center>
+            </header>
             </div>
+            <center>
+            <div class="card-content">
+                <p >
+                    You can share a Google Sheet containing student information (Name, ID, Email)<br>
+                    and  Create another Google Sheet containing questions 
+                    and corresponding marking rubrics in a specified format.<br>
+              
+                </p>
+            </div>
+            </center>
+        
+
+            
+
+        <div class="card mx-auto" style="width: 35%;">
+        <div class="card border-0">
+            <div class="card-header bg-primary text-center p-4">
+                <h2 class="text-white m-0">Start the Automatic Assessment Process</h2>
+            </div>
+            <div class="card-body rounded-bottom bg-white p-5">
+            <div class="card-body rounded-bottom bg-white p-5">
+              <form action="">
+                <div class="mb-3 row">
+                    <label for="info_url" class="form-label">URL of Information Sheet:</label>
+                    <input type="url" name="info_url" id="info_url" class="form-control" placeholder="info.Gsheet.com" required="required" />
+                </div>
+                <div class="mb-3 row">
+                    <label for="quest_url" class="form-label">URL of Question & Rubrics Sheet:</label>
+                    <input type="url" name="quest_url" id="quest_url" class="form-control" placeholder="question.Gsheet.com" required="required" />
+                </div>
+                <div class="mb-3 row">
+                    <label for="deadline" class="form-label">Submission Deadline:</label>
+                    <input type="date" name="deadline" id="deadline" class="form-control" placeholder="Submission Deadline" required="required" />
+                </div>
+                <div>
+                    <button class="btn btn-primary btn-lg btn-block" type="submit" id="submit" value="submit" name="submit">Assess the Students</button>
+                </div>
+
+              </form>
+            </div>
+            </div>
+        
+    </div>
+</div>
+</div>
+</div>
     </div>
 
 </div>
+</nav>
+
 </nav>
 </body>
 </html>
